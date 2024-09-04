e0405516721b78dae6cc156a3003096255563cb0
Remiasz <dremiasz@cps.k12.il.us>
2024-09-04 15:06:21 -0500

    string slicing

--- week2.py
+++ week2.py
@@ -1,5 +1,8 @@
-date = input("What day is it. ")
-print("Good to know it's " + date)
-holiday = input("Whats your favorite holiday ")
-print("I didn't know you liked " + holiday)
-print("Today is " + date + "and your favio es " + holiday) 
+# date = input("What day is it. ")
+# print("Good to know it's " + date)
+# holiday = input("Whats your favorite holiday ")
+# print("I didn't know you liked " + holiday)
+# print("Today is " + date + "and your favio es " + holiday) 
+# #string slicing
+word = "hello"
+print(word[1:4])
