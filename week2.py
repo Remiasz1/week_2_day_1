a3db52a74da7e61b9c8e051df0b3cd7eec007c6f
Remiasz <dremiasz@cps.k12.il.us>
2024-09-04 15:00:46 -0500

    first commit

---
+++ week2.py
@@ -0,0 +1,5 @@
+date = input("What day is it. ")
+print("Good to know it's " + date)
+holiday = input("Whats your favorite holiday ")
+print("I didn't know you liked " + holiday)
+print("Today is " + date + "and your favio es " + holiday) 
